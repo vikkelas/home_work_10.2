146a7e9cee552087bbb9e870f3d9427111974e28
vikkelas <bass89@mail.ru>
2021-11-25 19:48:36 +0300

    final

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,16 +1,13 @@
 /* eslint-disable linebreak-style */
 import json from './Json';
 import read from './Read';
-import SaveGaming from './GameSaving';
 
 export default class GameSavingLoader {
   static async load() {
     try {
       const readFile = await read();
       const jsonFile = await json(readFile);
-      const valueJson = JSON.parse(jsonFile);
-      const saveObj = new SaveGaming(valueJson.id, valueJson.created, valueJson.userInfo);
-      return saveObj;
+      return jsonFile;
     } catch (err) {
       return err.toString();
     }
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
+/* eslint-disable linebreak-style */
+import SaveGaming from './GameSaving';
+import GameSavingLoader from './GameSavingLoader';
 
+GameSavingLoader.load().then((value) => {
+  const obj = JSON.parse(value);
+  const save = new SaveGaming(obj.id, obj.created, obj.userInfo);
+  return save;
+});
